810a41fccc6cf5b3d46b8c206c888b8cae32d8f1
DevOsvaldoDevitm <osvaldo.maye@devitm.com>
2025-04-14 10:45:12 -0600

    Third commit

--- lib/shopify.ts
+++ lib/shopify.ts
@@ -1,10 +1,11 @@
 // lib/shopify.ts
 import { shopifyApi, LATEST_API_VERSION } from '@shopify/shopify-api'
+const scopes = process.env.SCOPES ? process.env.SCOPES.split(',') : [];
 
 export const shopify = shopifyApi({
   apiKey: process.env.SHOPIFY_API_KEY!,
   apiSecretKey: process.env.SHOPIFY_API_SECRET!,
-  scopes: process.env.SCOPES!.split(','),
+  scopes: scopes, // Usamos el valor correcto de scopes
   hostName: process.env.HOST!.replace(/^https?:\/\//, ''),
   apiVersion: LATEST_API_VERSION,
   isEmbeddedApp: false,
