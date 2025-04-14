5de5395b74a1be77df38346ed8cdaabb8885d6da
DevOsvaldoDevitm <osvaldo.maye@devitm.com>
2025-04-14 11:05:04 -0600

    Third commit

--- pages/api/auth/callback.ts
+++ pages/api/auth/callback.ts
@@ -9,8 +9,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     })
 
     res.status(200).json({ success: true, session })
-  } catch (e) {
-    console.error('Error en OAuth callback:', e)
-    res.status(500).json({ success: false, error: e })
+  } catch (e: unknown) {
+    // Verificamos que 'e' sea un objeto de error con un mensaje
+    if (e instanceof Error) {
+      console.error('Error en OAuth callback:', e.message)
+      res.status(500).json({ success: false, error: e.message })
+    } else {
+      // Si 'e' no es un Error, manejamos el caso
+      console.error('Error desconocido:', e)
+      res.status(500).json({ success: false, error: 'Error desconocido' })
+    }
   }
 }
